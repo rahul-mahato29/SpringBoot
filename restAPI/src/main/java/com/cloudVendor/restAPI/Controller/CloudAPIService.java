12d8c036d7cfda29e1a0e99f8abd6f1ca6c22850
Rahul Mahato <mahatorahul2901@gmail.com>
2025-03-25 10:49:53 +0530

    Initial commit

---
+++ restAPI/src/main/java/com/cloudVendor/restAPI/Controller/CloudAPIService.java
@@ -0,0 +1,36 @@
+package com.cloudVendor.restAPI.Controller;
+
+import com.cloudVendor.restAPI.Model.CloudVendor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudAPIService {
+    CloudVendor cloudVendor;
+
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId){
+//        return new CloudVendor("C1", "Vendor1",
+//                "Address1", "xxxxxx");
+
+        return cloudVendor;
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "CloudVendor Created Successfully";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "CloudVendor Details Updated Successfully";
+    }
+
+    @DeleteMapping
+    public String deleteCloudVendorDetails(){
+        this.cloudVendor = null;
+        return "CloudVendor Detail Deleted Successfully";
+    }
+}
